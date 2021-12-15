6fd10ba88f78b01c710c234504a43f3177cda3be
ljbaker <lewis@pymetrics.com>
2021-12-15 15:28:57 -0700

    updates to data class and naming

--- bartindr/data_grabbing.py
+++ bartindr/data_grabbing.py
@@ -10,55 +10,108 @@ import re
 
 from constants import *
 
-def get_and_preprocess_data():
+
+class BarData():
     """
-    Master function for retrieving and processing data. 
-    Accesses thecocktaildb api, retrieves data for all cocktails,
-    splits data into values and units, converts all values to mL, 
-    and then converts values to proportionate amounts.
-    
-    Still figuring out what to do with nonmetric units
-
-    Parameters
-    ----------
-    None
-
-
-    Returns
-    -------
-    prop_df : n*m pandas.DataFrame
-        dataframe of proportional ingredients by cocktail
-        n = cocktails
-        m = ingredients
-
-
-    Example
-    -------
-
-    An "A1" cocktail has the following row of ingredients
-        Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
-    A1  1 3/4 shot   1 Shot          1/4 Shot      1/8 Shot   1
-
-    shots are converted to mL and then standardized to be proportions summing to 1
-    count values are listed as TRUE
-        Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
-    A1  77.63        44.36           11.09         5.545      NONE
-
+    Class to retrieve and preprocess data for the bartindr project. Stores all
+    stages of data processing.
     """
-    
-    #TODO add data cache instead of calling API each time
-    print('identifying api endpoints...')
-    url_list = create_url_list()
-    print('pulling data...')
-    cocktail_df = get_cdb_data(url_list)
-    print('standardizing data...')
-    ml_df = standardize_measures_to_metric(cocktail_df, metric='mL')
-    print('converting to proportions...')
-    prop_df = standardize_measures_to_props(ml_df)
-    
-    return(prop_df)
-    
 
+    def __init__(self):
+        self.search_url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
+
+    def gather_data(self):
+        """
+        Master function for retrieving and processing data.
+        Accesses thecocktaildb api, retrieves data for all cocktails,
+        splits data into values and units, converts all values to mL,
+        and then converts values to proportionate amounts.
+
+        Still figuring out what to do with nonmetric units
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        prop_df : n*m pandas.DataFrame
+            dataframe of proportional ingredients by cocktail
+            n = cocktails
+            m = ingredients
+
+
+        Example
+        -------
+
+        An "A1" cocktail has the following row of ingredients
+            Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
+        A1  1 3/4 shot   1 Shot          1/4 Shot      1/8 Shot   1
+
+        shots are converted to mL and then standardized to be proportions summing to 1
+        count values are listed as TRUE
+            Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
+        A1  77.63        44.36           11.09         5.545      NONE
+        """
+
+        print('identifying api endpoints...')
+        self.url_list = create_url_list()
+        print('pulling data...')
+        self.cocktail_df = get_cdb_data(self.url_list)
+        print('standardizing data...')
+        self.standard_df = standardize_measures_to_metric(self.cocktail_df, metric='mL')
+        print('converting to proportions...')
+        self.prop_df = standardize_measures_to_props(self.standard_df)
+        print('standardizing column names...')
+        self.prop_df = _rename_columns(self.prop_df, column_semantics)
+
+# def get_and_preprocess_data():
+#     """
+#     Master function for retrieving and processing data.
+#     Accesses thecocktaildb api, retrieves data for all cocktails,
+#     splits data into values and units, converts all values to mL,
+#     and then converts values to proportionate amounts.
+#
+#     Still figuring out what to do with nonmetric units
+#
+#     Parameters
+#     ----------
+#     None
+#
+#     Returns
+#     -------
+#     prop_df : n*m pandas.DataFrame
+#         dataframe of proportional ingredients by cocktail
+#         n = cocktails
+#         m = ingredients
+#
+#
+#     Example
+#     -------
+#
+#     An "A1" cocktail has the following row of ingredients
+#         Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
+#     A1  1 3/4 shot   1 Shot          1/4 Shot      1/8 Shot   1
+#
+#     shots are converted to mL and then standardized to be proportions summing to 1
+#     count values are listed as TRUE
+#         Gin          Grand Marnier   Lemon Juice   Grenadine  Lemon Peel
+#     A1  77.63        44.36           11.09         5.545      NONE
+#
+#     """
+#
+#     print('identifying api endpoints...')
+#     url_list = create_url_list()
+#     print('pulling data...')
+#     cocktail_df = get_cdb_data(url_list)
+#     print('standardizing data...')
+#     standard_df = standardize_measures_to_metric(cocktail_df, metric='mL')
+#     print('converting to proportions...')
+#     prop_df = standardize_measures_to_props(standard_df)
+#     print('standardizing column names...')
+#     prop_df = _rename_columns(prop_df, column_semantics)
+#
+#     return(prop_df)
 
 def create_url_list():
     """
@@ -67,12 +120,12 @@ def create_url_list():
     Parameters
     ----------
     None
-    
+
     Returns
     -------
     url_list : list
         list of all possible search urls by first character
-        note: includes some urls that do not exist on API    
+        note: includes some urls that do not exist on API
     """
 
     url_list = []
@@ -107,13 +160,13 @@ def get_cdb_data(url_list):
     cocktail_df = pd.DataFrame()
 
     for url in url_list:
-        
+
         try:
             r = requests.get(url)
-            
+
             if r.json()['drinks']:
-                
-                for cocktail_dict in r.json()['drinks']: 
+
+                for cocktail_dict in r.json()['drinks']:
                     cdf = get_ingredients_from_cocktail(cocktail_dict)
                     cocktail_df = cocktail_df.append(cdf)
         except:
@@ -144,11 +197,12 @@ def get_ingredients_from_cocktail(cocktail_dict:dict) -> pd.DataFrame:
     ingredients = [i for i in cocktail_dict.keys() if i.find('strIngredient') > -1]
     ingredients_list = [cocktail_dict[i] for i in ingredients if i]
     ingredients_list = [t.lower() for t in ingredients_list if t]
+    # self.ingredients_list = ingredients_list
 
     measures = [i for i in cocktail_dict.keys() if i.find('strMeasure') > -1]
     measures_list = [cocktail_dict[m] for m in measures if m]
     measures_list = [t.lower() for t in measures_list if t]
-
+    # self.measures_list = measures_list
 
     cdf = pd.DataFrame(dict(zip(ingredients_list,measures_list)), index=[cocktail_dict['strDrink']])
 
@@ -159,17 +213,17 @@ def get_ingredients_from_cocktail(cocktail_dict:dict) -> pd.DataFrame:
 def split_value_measure(measure):
     """
     helper function to divide values from units in cocktail measuremets by individual cell
-    
+
     Parameters
     ----------
-    row : pandas.Series
-        row of values from a pandas.DataFrame
-    
+    row : str
+        string of numeric and characters
+
     Returns
     -------
     val : numeric
         numeric value of corresponding unit
-    unit : string
+    unit : str
         string unit of corresponding value
     """
 
@@ -186,7 +240,7 @@ def split_value_measure(measure):
 
     if not unit:
         unit = 'count' #cases where unit is implied, e.g. "Lemon: 1"
-        
+
     #take abs in case of misinterpretation of m-dash as negative
     return(abs(val), unit)
 
@@ -194,12 +248,12 @@ def split_value_measure(measure):
 def split_value_measure_byrow(row):
     """
     helper function to divide units from measurements in cocktails by row
-    
+
     Parameters
     ----------
     row : pandas.Series
         row of values from a pandas.DataFrame
-    
+
     Returns
     -------
     values : list(numeric)
@@ -207,18 +261,18 @@ def split_value_measure_byrow(row):
     units : list(string)
         list of strings corresponding to units of values
     """
-    
+
     cleanrow = row.dropna()
     values = []
     units = []
-    
+
     for i, v in cleanrow.iteritems():
-        
+
         val, unit = split_value_measure(v)
         # convert_unit
         values += [val]
         units += [unit]
-    
+
     return(values, units)
 
 
@@ -226,7 +280,7 @@ def convert_unit(value, input_unit, conversion='mL'):
     """
     convert values of input_units to conversion units
     only supports conversion to mL at the moment
-    
+
     Parameters
     ----------
     value : numeric
@@ -236,7 +290,7 @@ def convert_unit(value, input_unit, conversion='mL'):
     conversion : str
         unit to convert input_unit value
         only mL is supported at the moment
-    
+
     Returns
     -------
     c_values : numeric
@@ -260,7 +314,7 @@ def batch_convert_units(values, input_units, conversion='mL'):
     """
     convert multiple value-unit pairs to conversion units
     only supports conversion to mL at the moment
-    
+
     Parameters
     ----------
     values : list(numeric)
@@ -270,24 +324,24 @@ def batch_convert_units(values, input_units, conversion='mL'):
     conversion : str
         unit to convert input_unit-value pairs
         only mL is supported at the moment
-    
+
     Returns
     -------
     c_values : numeric
         values converted to conversion metric
     """
-    
+
     c_values = []
     for v, u in zip(values, input_units):
         c_values += [convert_unit(value=v, input_unit=u, conversion=conversion)]
-        
+
     return(c_values)
 
 def standardize_measures_to_props(converted_df):
     """
-    Convert uniform measures of ingredients (e.g., all ingredients listed in mL) 
+    Convert uniform measures of ingredients (e.g., all ingredients listed in mL)
     to proportionate amounts per drink
-    
+
     Still figuring out nonmetric units
 
     Parameters
@@ -320,7 +374,7 @@ def standardize_measures_to_props(converted_df):
 def standardize_measures_to_metric(df, metric='mL'):
     """
     Convert raw measures of ingredients to milliliters
-    
+
     Still figuring out what to do with nonmetric units
 
     Parameters
@@ -328,7 +382,7 @@ def standardize_measures_to_metric(df, metric='mL'):
     df : pandas.DataFrame
         dataframe of value-metric pairs, exported from get_cdb_data
     metric: str
-        metric to convert value-metric pairs in df. 
+        metric to convert value-metric pairs in df.
         currently only supports mL conversions
 
     Returns
@@ -351,7 +405,7 @@ def standardize_measures_to_metric(df, metric='mL'):
 
     """
     converted_df = pd.DataFrame()
-    
+
     for index, row in df.iterrows():
         row = row.dropna()
         # break out values and units
@@ -361,14 +415,14 @@ def standardize_measures_to_metric(df, metric='mL'):
         c_values = batch_convert_units(values, c_units, conversion='mL')
         temp = pd.DataFrame(dict(zip(ingredients, c_values)), index=[index])
         converted_df = converted_df.append(temp)
-        
+
     return(converted_df)
-        
-        
+
+
 def find_unit(unit_string, verbose=False):
     """
     find correct unit embedded in larger string
-    
+
     Parameters
     ----------
     unit_string : str
@@ -382,19 +436,19 @@ def find_unit(unit_string, verbose=False):
     unit : str
         structured unit from list of acceptable units for converstion
         if no units match the unit_string, will return None
-        
+
     """
-    
+
     item = _remove_spaces(unit_string)
     unit = None
-    
+
     if item in (metric_words + odd_metric_words + part_words):
         unit = item
-        
+
     elif item in (nonmetric_words):
         # separating non-metric words for future editing
         unit = item
-        
+
     else:
         #imperfect matches
         #prioritizing primary metrics
@@ -418,19 +472,19 @@ def find_unit(unit_string, verbose=False):
                     if verbose: print(f'no exact match found for [{item}]; using [{unit}]')
 
     if (not unit) and verbose: print(f'no units for [{item}]')
-        
-    return(unit)  
+
+    return(unit)
 
 
 def _check_separate_numeric(string):
     """
     helper function to make sure that numeric values have a space between alphabetical characters
-    
+
     Parameters
     ----------
     string : str
         any string
-        
+
     Returns
     -------
     new_string : str
@@ -456,12 +510,12 @@ def _remove_spaces(string):
     """
     verifies that no spaces exist in string
     removes spaces if found
-    
+
     Parameters
     ----------
     string : str
         any string
-    
+
     Returns
     -------
     new_string : str
@@ -473,15 +527,24 @@ def _remove_punctuation(string):
     """
     remove punctuation from strings
     necessary for separating numerics from alphabetic characters
-    
+
     Parameters
     ----------
     string : str
         any string
-    
+
     Returns
     -------
     new_string : str
         string with punctuation replaced with a space
     """
     return re.sub(r"[+,.;@#?!&$-]+\ *", " ", string)
+
+def _rename_columns(df, column_semantics):
+    """
+    helper function to rename the ouput dataframe into common semantics
+    e.g., anisette, sambuca and ouzo are all types of anise liqueur,
+    """
+    simple_df = df.rename(columns=column_semantics)
+    simple_df = simple_df.groupby(simple_df.columns, axis=1).sum()
+    return(simple_df)
